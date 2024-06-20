f7a4fba98358dbd56d8446cad4a187a87e015e10
wendelleloi <wendelleloi2@gmail.com>
2024-06-20 16:46:29 -0300

    feat: install shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import { Button } from "./components/ui/button"
+import "./global.css"
+
 export function App() {
   return (
-    <h1>hello</h1>
+    <div className="flex justify-center items-center h-screen">
+      <Button>Enviar</Button>
+    </div>
   )
 }
