37cb34ecf38b7eeaac67f483df8e32ec2fb209a6
wendelleloi <wendelleloi2@gmail.com>
2024-06-29 11:12:28 -0300

    feat: conect authenticate with api

---
+++ src/api/sig-in.ts
@@ -0,0 +1,11 @@
+import { api } from '@/lib/axios'
+
+export interface SigInBody {
+  email: string
+}
+
+export async function sigIn({ email }: SigInBody) {
+  await api.post('authenticate', {
+    email,
+  })
+}
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,12 @@
 import { Label } from '@radix-ui/react-label'
+import { useMutation } from '@tanstack/react-query'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
+import { sigIn } from '@/api/sig-in'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 
@@ -21,11 +23,15 @@ export function SignIn() {
     formState: { isSubmitting },
   } = useForm<signInForm>()
 
+  const { mutateAsync: authenticate } = useMutation({
+    mutationFn: sigIn,
+  })
+
   async function handleSignIn(data: signInForm) {
     try {
       console.log(data)
 
-      await new Promise((resolve) => setTimeout(resolve, 2000))
+      await authenticate({ email: data.email })
 
       toast.success('Enviamos um link de autenticação para seu e-mail.', {
         action: {
